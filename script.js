ae3ee16863d617de4b40ae495ab093248231a932
ali85k <aliceanndraad56@gmail.com>
2022-07-12 15:58:53 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let terror = [{
+    title: 'Titane',
+    diretor: 'Julia Ducournau',
+    year: 2021
+}]
+
+let action = [{
+    title: 'Corra!',
+    diretor:'Jordan Peele',
+    year:2017
+}]
+
+let fantasy = [{
+    title: 'Alice no Pais das Maravilhas',
+    diretor: 'Tim Burton',
+    year: 2010
+}]
